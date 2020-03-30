7e7aa47a586b4e02c0d24e25a5418b9698caf9a1
Aryan Jharwal <55040202+aryanjharwal@users.noreply.github.com>
2020-03-29 23:44:44 -0400

    Update Application.java

--- jharwal_problem2/src/jharwal_problem2/Application.java
+++ jharwal_problem2/src/jharwal_problem2/Application.java
@@ -1,28 +1,20 @@
 package jharwal_problem2;
 
-import java.io.IOException;
-
-public class Application {
-
-	   public static void main(String[] args) {
-
-	       String dataFile = "problem2.txt";
-	       String outputFile = "unique_word_counts.txt";
-
-	       DuplicateCounter duplicateCounter = new DuplicateCounter();
-
-	       try {
-	           duplicateCounter.count(dataFile);
-	       } catch (IOException e) {
-	           e.printStackTrace();
-	       }
-
-	       try {
-	           duplicateCounter.write(outputFile);
-	       } catch (IOException e) {
-	           e.printStackTrace();
-	       }
-
-	   }
-
-	}
+//Declare class called Application
+public class Application
+{
+//main method
+public static void main(String[] args)
+{
+//define the string variable and store the input file name
+String input_file = "problem2.txt";
+//define the string variable and store the output file name
+String output_file = "unique_word_counts.txt";
+//Create object for the class DuplicateCounter
+DuplicateCounter DupCouObj = new DuplicateCounter();
+//call the count method using the class object
+DupCouObj.count(input_file);
+//call the write method using the class object
+DupCouObj.write(output_file);
+}
+}
